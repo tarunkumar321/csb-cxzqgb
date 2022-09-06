7366c86956fa3bd4f4750f1f1c4f5727cf4dc1f9
Code Sandbox <hello@codesandbox.io>
2022-09-06 07:16:27 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9c3eb375858d07bb5d78abb529afc5e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fbf1261158aec291ca3c55be5d71fc",
+    "url": "https://tarunkumar321.github.io/csb-cxzqgb/index.html"
+  },
+  {
+    "revision": "4f36702f24a76a8b0cdb",
+    "url": "https://tarunkumar321.github.io/csb-cxzqgb/static/css/main.5a926c7a.chunk.css"
+  },
+  {
+    "revision": "3d6ea18f9c53d9d15d6c",
+    "url": "https://tarunkumar321.github.io/csb-cxzqgb/static/js/2.4aa973c4.chunk.js"
+  },
+  {
+    "revision": "4f36702f24a76a8b0cdb",
+    "url": "https://tarunkumar321.github.io/csb-cxzqgb/static/js/main.eb4d81a8.chunk.js"
+  },
+  {
+    "revision": "568e4516c155d540d12e",
+    "url": "https://tarunkumar321.github.io/csb-cxzqgb/static/js/runtime~main.8c85f264.js"
+  }
+]);
